cb1213347deb2ae61630327498a1177171521e2e
wwykahuna <89023889@qq.com>
2021-09-08 07:49:19 +0800

    Create AsyncTaskProperty.java

---
+++ src/main/java/com/sitech/crmmci/eai/brupet/config/properties/AsyncTaskProperty.java
@@ -0,0 +1,58 @@
+package com.sitech.crmpd.quant.config.properties;
+
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties(prefix = "async.task")
+public class AsyncTaskProperty {
+
+    private int corePoolSize = 10;
+
+    private int maxPoolSize = 20;
+
+    private int queueCapacity = 2000;
+
+    private int keepAliveSeconds = 60;
+
+    private String threadNamePrefix = "async-task-";
+
+    public int getCorePoolSize() {
+        return corePoolSize;
+    }
+
+    public void setCorePoolSize(int corePoolSize) {
+        this.corePoolSize = corePoolSize;
+    }
+
+    public int getMaxPoolSize() {
+        return maxPoolSize;
+    }
+
+    public void setMaxPoolSize(int maxPoolSize) {
+        this.maxPoolSize = maxPoolSize;
+    }
+
+    public int getQueueCapacity() {
+        return queueCapacity;
+    }
+
+    public void setQueueCapacity(int queueCapacity) {
+        this.queueCapacity = queueCapacity;
+    }
+
+    public int getKeepAliveSeconds() {
+        return keepAliveSeconds;
+    }
+
+    public void setKeepAliveSeconds(int keepAliveSeconds) {
+        this.keepAliveSeconds = keepAliveSeconds;
+    }
+
+    public String getThreadNamePrefix() {
+        return threadNamePrefix;
+    }
+
+    public void setThreadNamePrefix(String threadNamePrefix) {
+        this.threadNamePrefix = threadNamePrefix;
+    }
+}
